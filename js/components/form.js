cc02314cce79088875875645c4c43da7041ab131
Slabnevitch <handofschpilrain@mail.ru>
2025-07-07 23:20:15 +0500

    отвалидировал input[type='tel']

--- js/components/form.js
+++ js/components/form.js
@@ -2,4 +2,7 @@ export const inputReset = () => {
 	$('.reset, .inp-reset').on('click', function(e) {
 		$(this).prev('.form__input').val('');
 	});
+	$('input[type="tel"]').on('input', function () {
+	  this.value = this.value.replace(/[^0-9]/g, '');
+	});
 }
